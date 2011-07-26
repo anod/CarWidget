099399486d9d879e926cf907f98c291ba2796b52
a_n_o_d <a_n_o_d@dfbe8a62-d448-48c2-a5a9-fd57a4a07a30>
2011-07-26 04:14:49 +0000

    edit button

--- CarHomeWidget/src/com/anod/car/home/prefs/views/IconPreference.java
+++ CarHomeWidget/src/com/anod/car/home/prefs/views/IconPreference.java
@@ -5,12 +5,14 @@ import android.graphics.Bitmap;
 import android.preference.Preference;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
 import com.anod.car.home.R;
 
-public class IconPreference extends Preference {
+public class IconPreference extends Preference implements OnClickListener {
 	private Bitmap mIconBitmap;
+	private Boolean mShowEditButton = false;
 	
     public IconPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -29,6 +31,7 @@ public class IconPreference extends Preference {
     
     public void setIconBitmap(Bitmap iconBitmap) {
     	mIconBitmap = iconBitmap;
+    	mShowEditButton = true;
     	notifyChanged();
     }
     
@@ -39,6 +42,22 @@ public class IconPreference extends Preference {
         ImageView imageView = (ImageView)view.findViewById(R.id.pref_icon_view);
 	    if (imageView != null && mIconBitmap != null) { 
 	    	imageView.setImageBitmap(mIconBitmap);
+	    } 
+        ImageView editButton = (ImageView)view.findViewById(R.id.edit_action_button);
+        View divider = (View)view.findViewById(R.id.divider);
+	    if (mShowEditButton) {
+	    	editButton.setOnClickListener(this);
+	    	editButton.setVisibility(View.VISIBLE);
+	    	divider.setVisibility(View.VISIBLE);
+	    } else {
+	    	editButton.setVisibility(View.GONE);
+	    	divider.setVisibility(View.GONE);
 	    }
     }
+
+	@Override
+	public void onClick(View v) {
+		// TODO
+		
+	}
 }
