60fd4626f94c175e519c3fdd4748e3b4eeb546e8
a_n_o_d <a_n_o_d@dfbe8a62-d448-48c2-a5a9-fd57a4a07a30>
2011-10-19 07:27:02 +0000

    update configuration

--- CarHomeWidgetFree/gen/com/anod/car/home/free/R.java
+++ CarHomeWidgetFree/gen/com/anod/car/home/free/R.java
@@ -31,16 +31,18 @@ public final class R {
     public static final class dimen {
         public static final int delete_zone_padding=0x7f070005;
         public static final int delete_zone_size=0x7f070004;
-        public static final int dots_height=0x7f07000a;
-        public static final int dots_width=0x7f07000b;
+        public static final int dots_height=0x7f07000c;
+        public static final int dots_width=0x7f07000d;
         public static final int half_status_bar_height=0x7f070003;
         public static final int hint_bubble_offset_horizontal=0x7f070001;
         public static final int hint_bubble_offset_vertical=0x7f070002;
         public static final int icon_size=0x7f070000;
-        public static final int win7_cell_height=0x7f070009;
-        public static final int win7_cell_width=0x7f070008;
-        public static final int workspace_cell_height=0x7f070007;
-        public static final int workspace_cell_width=0x7f070006;
+        public static final int title_padding_bottom=0x7f070006;
+        public static final int title_padding_bottom_win7=0x7f070007;
+        public static final int win7_cell_height=0x7f07000b;
+        public static final int win7_cell_width=0x7f07000a;
+        public static final int workspace_cell_height=0x7f070009;
+        public static final int workspace_cell_width=0x7f070008;
     }
     public static final class drawable {
         public static final int app_header_bg=0x7f020000;
