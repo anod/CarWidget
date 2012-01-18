6316c82c1e2eab58e4ac8afb2593f992fb40153d
a_n_o_d <a_n_o_d@dfbe8a62-d448-48c2-a5a9-fd57a4a07a30>
2012-01-18 18:29:15 +0000

    change all layouts to be resizable

--- CarHomeWidget/src/com/anod/car/home/utils/Utils.java
+++ CarHomeWidget/src/com/anod/car/home/utils/Utils.java
@@ -2,6 +2,7 @@ package com.anod.car.home.utils;
 
 import android.content.ComponentName;
 
+
 public class Utils {
 	public static float calcIconsScale(String scaleString) {
         return 1.0f+0.1f*Integer.valueOf(scaleString);
