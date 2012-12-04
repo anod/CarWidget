0fa6a5e3a55ad69d2094981fa542d0bfcf474b7d
anod <alex.gavrishev@gmail.com>
2012-12-04 21:06:35 +0200

    Revert "convert left right to start end"
    This reverts commit 64af7956b0128268d2078be8675ba9cfce0ad4f8.

--- CarHomeWidget/src/com/anod/car/home/ShortcutActivity.java
+++ CarHomeWidget/src/com/anod/car/home/ShortcutActivity.java
@@ -14,9 +14,9 @@ import com.anod.car.home.appwidget.ShortcutPendingIntent;
 public class ShortcutActivity extends Activity {
 	public static final String EXTRA_INTENT = "intent";
 	public static final String EXTRA_MEDIA_BUTTON = "media_button";
-
+	
 	public static final String ACTION_MEDIA_BUTTON = "action_media_button";
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,20 +35,20 @@ public class ShortcutActivity extends Activity {
 	}
 
 	private void broadCastMediaCode(Integer keyCode) {
-		Intent mediaButtonDownIntent = new Intent(Intent.ACTION_MEDIA_BUTTON);
-		KeyEvent downKe = new KeyEvent(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), KeyEvent.ACTION_DOWN, keyCode, 0);
-		mediaButtonDownIntent.putExtra(Intent.EXTRA_KEY_EVENT, downKe);
+        Intent mediaButtonDownIntent = new Intent(Intent.ACTION_MEDIA_BUTTON);
+        KeyEvent downKe = new KeyEvent(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), KeyEvent.ACTION_DOWN, keyCode, 0);
+        mediaButtonDownIntent.putExtra(Intent.EXTRA_KEY_EVENT, downKe);
 
-		Intent mediaButtonUpIntent = new Intent(Intent.ACTION_MEDIA_BUTTON);
-		KeyEvent upKe = new KeyEvent(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), KeyEvent.ACTION_UP, keyCode, 0);
-		mediaButtonUpIntent.putExtra(Intent.EXTRA_KEY_EVENT, upKe);
-
-		sendOrderedBroadcast(mediaButtonDownIntent, null, null, null, Activity.RESULT_OK, null, null);
-		sendOrderedBroadcast(mediaButtonUpIntent, null, null, null, Activity.RESULT_OK, null, null);
+        Intent mediaButtonUpIntent = new Intent(Intent.ACTION_MEDIA_BUTTON);
+        KeyEvent upKe = new KeyEvent(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), KeyEvent.ACTION_UP, keyCode, 0);
+        mediaButtonUpIntent.putExtra(Intent.EXTRA_KEY_EVENT, upKe);
+        
+        sendOrderedBroadcast(mediaButtonDownIntent, null, null, null, Activity.RESULT_OK, null, null);
+        sendOrderedBroadcast(mediaButtonUpIntent, null, null, null, Activity.RESULT_OK, null, null);
 	}
 
 	private void runFromIntent(Intent intent) {
-		// fix for Galaxy s3
+		//fix for Galaxy s3
 		String action = intent.getAction();
 		if (action != null && action.equals(ShortcutPendingIntent.INTENT_ACTION_CALL_PRIVILEGED)) {
 			intent.setAction(Intent.ACTION_CALL);
@@ -56,18 +56,20 @@ public class ShortcutActivity extends Activity {
 		if (intent.getSourceBounds() == null) {
 			intent.setSourceBounds(getIntent().getSourceBounds());
 		}
-
+		
 		startActivitySafetly(intent);
 	}
-
-	private void startActivitySafetly(Intent intent) {
-		try {
-			startActivity(intent);
-		} catch (ActivityNotFoundException e) {
-			Toast.makeText(this, getString(R.string.activity_not_found), Toast.LENGTH_SHORT).show();
-		} catch (SecurityException e) {
-			Toast.makeText(this, getString(R.string.activity_not_found), Toast.LENGTH_SHORT).show();
-			Log.e("CarHomeWidget", "Widget does not have the permission to launch " + intent + ". Make sure to create a MAIN intent-filter for the corresponding activity " + "or use the exported attribute for this activity.", e);
-		}
-	}
+	
+    private void startActivitySafetly(Intent intent) {
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, getString(R.string.activity_not_found), Toast.LENGTH_SHORT).show();
+        } catch (SecurityException e) {
+            Toast.makeText(this, getString(R.string.activity_not_found), Toast.LENGTH_SHORT).show();
+            Log.e("CarHomeWidget", "Widget does not have the permission to launch " + intent +
+                    ". Make sure to create a MAIN intent-filter for the corresponding activity " +
+                    "or use the exported attribute for this activity.", e);
+        }
+    }	
 }
