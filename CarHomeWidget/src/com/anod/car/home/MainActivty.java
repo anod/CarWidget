97f2f0f541d01afd70efff64f65551b28514bef2
anod <alex.gavrishev@gmail.com>
2013-05-23 07:06:59 +0300

    add main activity

---
+++ CarHomeWidget/src/com/anod/car/home/MainActivty.java
@@ -0,0 +1,15 @@
+package com.anod.car.home;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author alex
+ * @date 5/22/13
+ */
+public class MainActivty extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activty);
+	}
+}
