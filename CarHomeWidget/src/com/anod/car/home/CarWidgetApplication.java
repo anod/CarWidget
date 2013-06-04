008213af0e6eac47b5ba66cd7f00ebc266d0b3d1
anod <alex.gavrishev@gmail.com>
2013-06-04 06:48:42 +0300

    init ACRA

--- CarHomeWidget/src/com/anod/car/home/CarWidgetApplication.java
+++ CarHomeWidget/src/com/anod/car/home/CarWidgetApplication.java
@@ -3,26 +3,26 @@ package com.anod.car.home;
 import com.anod.car.home.model.AppsListCache;
 
 import android.app.Application;
+import org.acra.ACRA;
 import org.acra.ReportField;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
 @ReportsCrashes(formKey = "", // will not be used
 		mailTo = "alex.gavrishev@gmail.com",
-		customReportContent = {
-				ReportField.APP_VERSION_CODE,
-				ReportField.APP_VERSION_NAME,
-				ReportField.ANDROID_VERSION,
-				ReportField.PHONE_MODEL,
-				ReportField.CUSTOM_DATA,
-				ReportField.STACK_TRACE,
-				ReportField.LOGCAT
-		},
 		mode = ReportingInteractionMode.TOAST,
 		resToastText = R.string.crash_toast_text)
 public class CarWidgetApplication extends Application {
 	public AppsListCache mAppListCache;
 	public AppsListCache mIconThemesCache;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// The following line triggers the initialization of ACRA
+		ACRA.init(this);
+	}
 
 	public AppsListCache getAppListCache() {
 		return mAppListCache;
