694458a0aca32ffee168ecc0d3f81c2291549610
Alex Gavrishev <alex.gavrishev@gmail.com>
2015-08-29 10:23:56 +0300

    fix log crash

--- CarHomeWidget/src/main/java/com/anod/car/home/utils/AppLog.java
+++ CarHomeWidget/src/main/java/com/anod/car/home/utils/AppLog.java
@@ -43,7 +43,7 @@ public class AppLog {
 
     private static String format(final String msg, final Object... array) {
         String formatted;
-        if (array == null) {
+        if (array == null || array.length == 0) {
             formatted = msg;
         } else {
             try {
