04e29b8d615c0f8135514b7a054d8a30350fac5f
Alex Gavrishev <alex.gavrishev@gmail.com>
2020-09-26 20:00:18 +0300

    Upgrade libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,36 +17,36 @@ plugins {
 
 dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("com.google.android.material:material:1.2.0-beta01")
+    implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.preference:preference:1.1.1")
     implementation("androidx.palette:palette:1.0.0")
 
     implementation("com.squareup.picasso:picasso:2.71828")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 
     // Activity recognition
     implementation("com.google.android.gms:play-services-location:17.0.0")
 
-    implementation("androidx.core:core-ktx:1.3.0")
-    implementation("androidx.fragment:fragment-ktx:1.2.4")
+    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("androidx.fragment:fragment-ktx:1.2.5")
     implementation("androidx.collection:collection-ktx:1.1.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
     implementation("androidx.palette:palette-ktx:1.0.0")
     implementation("androidx.sqlite:sqlite-ktx:2.1.0")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.0.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.2.0")
     kapt("androidx.lifecycle:lifecycle-compiler:2.2.0")
 
-    implementation("ch.acra:acra-core:5.5.1")
-    implementation("ch.acra:acra-notification:5.5.1")
-    implementation("ch.acra:acra-limiter:5.5.1")
+    implementation("ch.acra:acra-core:5.7.0")
+    implementation("ch.acra:acra-notification:5.7.0")
+    implementation("ch.acra:acra-limiter:5.7.0")
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.72")
-    implementation("com.android.support.constraint:constraint-layout:2.0.0-beta6")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10")
+    implementation("com.android.support.constraint:constraint-layout:2.0.1")
 }
 
 android {
