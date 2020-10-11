b6b465617443a4e3d8ab287b6ac920131539e491
Alex Gavrishev <alex.gavrishev@gmail.com>
2020-10-11 23:22:26 +0300

    Implement field update

--- compose/src/main/java/info/anodsplace/carwidget/prefs/IntentEditViewModel.kt
+++ compose/src/main/java/info/anodsplace/carwidget/prefs/IntentEditViewModel.kt
@@ -12,14 +12,42 @@ class IntentEditViewModel(application: Application): AndroidViewModel(applicatio
     val action = SingleLiveEvent<UiAction>()
 
     fun updateField(field: IntentField) {
-        val intent = intent.value!!
+        val current = intent.value!!
         when (field) {
             is IntentField.Action -> {
-                intent.action = field.value
+                current.action = field.value
             }
             is IntentField.Data -> {
-                intent.data = field.uri
+                current.data = field.uri
+            }
+            is IntentField.Categories -> {
+                val existing = current.categories ?: emptySet()
+                val new = field.value ?: emptySet()
+                (existing - new).forEach { current.removeCategory(it) }
+                new.forEach {
+                    if (!current.hasCategory(it)) {
+                        current.addCategory(it)
+                    }
+                }
+            }
+            is IntentField.Component -> {
+                val comp = field.value
+                if (comp == null || (comp.packageName.isBlank() && comp.className.isBlank())) {
+                    current.component = null
+                } else if (comp.packageName.isNotBlank() || comp.className.isNotBlank()) {
+                    current.component = comp
+                }
+            }
+            is IntentField.Extras -> {
+                current.putExtras(field.bundle)
+            }
+            is IntentField.Flags -> {
+                current.flags = field.value
+            }
+            is IntentField.MimeType -> {
+                current.type = field.value
             }
         }
+        intent.value = current
     }
 }
