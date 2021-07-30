f8055f8266d45c224cd776aafc35e065c9ef07a3
Alexandr Gavrishev <alex.gavrishev@gmail.com>
2021-07-30 23:31:23 +0300

    Compose 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,17 +23,17 @@ dependencies {
     implementation("io.insert-koin:koin-core:3.0.2")
     implementation("com.squareup.picasso:picasso:2.8")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1")
 
     // Activity recognition
     implementation("com.google.android.gms:play-services-location:18.0.0")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.palette:palette-ktx:1.0.0")
     implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.activity:activity:1.2.3")
-    implementation("androidx.fragment:fragment:1.3.5")
-    implementation("androidx.fragment:fragment-ktx:1.3.5")
+    implementation("androidx.activity:activity:1.3.0")
+    implementation("androidx.fragment:fragment:1.3.6")
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
     implementation("androidx.collection:collection-ktx:1.1.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 
     composeOptions {
         kotlinCompilerVersion = "1.5.10"
-        kotlinCompilerExtensionVersion = "1.0.0-rc02"
+        kotlinCompilerExtensionVersion = "1.0.0"
     }
 }
 
@@ -47,20 +47,20 @@ dependencies {
     implementation("com.squareup.picasso:picasso:2.8")
 
     implementation("androidx.appcompat:appcompat:1.4.0-alpha03")
-    implementation("androidx.fragment:fragment-ktx:1.3.5")
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
 
-    implementation("androidx.compose.ui:ui:1.0.0-rc02")
-    implementation("androidx.compose.foundation:foundation:1.0.0-rc02")
-    implementation("androidx.compose.material:material:1.0.0-rc02")
-    implementation("androidx.compose.material:material-icons-core:1.0.0-rc02")
-    implementation("androidx.compose.material:material-icons-extended:1.0.0-rc02")
+    implementation("androidx.compose.ui:ui:1.0.0")
+    implementation("androidx.compose.foundation:foundation:1.0.0")
+    implementation("androidx.compose.material:material:1.0.0")
+    implementation("androidx.compose.material:material-icons-core:1.0.0")
+    implementation("androidx.compose.material:material-icons-extended:1.0.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha04")
-    implementation("androidx.activity:activity-compose:1.3.0-rc02")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha05")
+    implementation("androidx.activity:activity-compose:1.3.0")
 
     implementation("com.google.accompanist:accompanist-pager:0.11.1")
 
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-rc02")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.10")
 }
